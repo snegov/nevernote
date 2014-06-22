6a818f4bb46989903d42392d31e3d6f0b9e5bc09
Maks Snegov <snegovik@gmail.com>
2014-06-23 00:50:21 +0400

    fix: error with empty GET urls

--- nevernote.py
+++ nevernote.py
@@ -56,7 +56,8 @@ def download_content(url, depth=0):
     else:
         raise NotImplementedError("protocol %s is not implemented" % up.scheme)
 
-    conn.request("GET", '?'.join((up.path, up.query)), None, headers)
+    requrl = ('?'.join((up.path, up.query)) if up.query else up.path) or '/'
+    conn.request("GET", requrl, None, headers)
     response = conn.getresponse()
 
     # follow redirects
